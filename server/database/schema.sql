33de42125fbf664001b99bb53266429e09b93d47
ANTOINE FOUGA <antoine.fouga1@gmail.com>
2024-08-26 14:43:44 +0200

    quest 5

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,18 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+create table category (
+  id int UNSIGNED primary key auto_increment not null,
+  name varchar(255) not null
+);
+
+create table program (
+ id int primary key auto_increment not null,
+  title varchar(255) not null,
+  synopsis TEXT not null,
+  poster varchar(255) not null,
+  country varchar(255) not null,
+  year int not null,
+  category_id INT UNSIGNED NOT NULL,
+  foreign key(category_id) references category(id)
+)
